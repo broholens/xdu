9c60500c3a4f8172bcff339668ed7d49a83044e9
broholens <broholens@outlook.com>
2017-09-26 02:12:49 +0800

    add settings

---
+++ ipproxy/settings.py
@@ -0,0 +1,31 @@
+
+# website validators
+
+WEBSITES = [
+    'https://www.baidu.com/',
+    'https://cn.bing.com/',
+    'http://www.firefox.com.cn/',
+    'http://www.asus.com.cn/'
+    'https://www.douban.com/',
+    'https://www.jd.com/',
+    'https://www.1688.com/',
+    'http://www.sina.com.cn/',
+    'http://www.qq.com/'
+]
+
+TEST_SITES = [
+    'http://httpbin.org/ip',
+    'http://2017.ip138.com/ic.asp',
+    'http://www.iprivacytools.com/proxy-checker-anonymity-test/',
+    'http://www.proxylists.net/proxyjudge.php'
+]
+
+PROXY_SITES = [
+    'https://www.rmccurdy.com/scripts/proxy/good.txt',
+    'http://www.proxylists.net/http_highanon.txt',
+    'http://ab57.ru/downloads/proxyold.txt',
+
+]
+
+# mongouri
+MONGO = 'mongodb://localhost:27017'
