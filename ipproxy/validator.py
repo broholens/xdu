1d5e36773054cecba25bb3f4be9c0ad6c79d4e40
broholens <broholens@outlook.com>
2017-09-26 22:09:53 +0800

    rewrite ip judge logic

--- ipproxy/validator.py
+++ ipproxy/validator.py
@@ -6,7 +6,7 @@ created at 2017-9-25 by broholens
 
 import random
 from ipproxy.settings import (DB, TEST_SITES, HOST, WEBSITES)
-from ipproxy.utils import request, exe_tasks
+from ipproxy.utils import request, exe_tasks, logger
 
 
 class Validator:
@@ -14,16 +14,13 @@ class Validator:
     def __init__(self):
         # result of httpbin is different from ip181
         self.db = DB
+        self.logger = logger
         self.validator = {
-            'good': ip_validator,
-            'available': website_validator
+            'good': self.ip_validator,
+            'available': self.website_validator
         }
 
-    def validate(self, proxy_list):
-        self._validate(proxy_list)
-        self._validate(proxy_list, 'available')
-
-    def _validate(self, proxy_list, key='good'):
+    def validate(self, proxy_list, key='good'):
         for proxy in exe_tasks(self.validator.get(key), proxy_list):
             self.storage(proxy, key)
 
@@ -39,29 +36,37 @@ class Validator:
             upsert=True
         )
 
-
-def ip_validator(proxy):
-    """
-    validate ip usable by some websites that return ip
-    :param proxy: something like {'http': 'http://162.243.107.120:3128'}
-    :return: proxy if usable else None
-    """
-    if proxy is None:
-        return None
-    url = random.choice(TEST_SITES)
-    resp = request(url, proxy)
-    if not resp:
-        return None
-    matches = HOST.search(resp.text)
-    if matches and matches.group(0) in proxy.get('http'):
+    def ip_validator(self, proxy):
+        """
+        validate ip usable by some websites that return ip
+        :param proxy: something like {'http': 'http://162.243.107.120:3128'}
+        :return: proxy if usable else None
+        """
+        if proxy is None:
+            return None
+        url = random.choice(TEST_SITES)
+        resp = request(url, proxy)
+        if not resp:
+            return None
+        self.logger.info('ip validator: available proxy - %s', proxy)
         return proxy
+        # distinguish anonymous level
+        # matches = HOST.findall(resp.text)
+        # if proxy.get('http').split(':')[1][2:] in matches:
+        #     self.logger.info('ip validator: %s', proxy)
+        #     return proxy
+        # self.logger.error('? %s', resp.text)
+        # self.logger.error('ip validator: useless ip %s', proxy)
 
-
-def website_validator(proxy):
-    """
-    likes ip_validator except uses website like baidu
-    """
-    url = random.choice(WEBSITES)
-    resp = request(url, proxy)
-    if resp and resp.status_code == 200:
-        return proxy
+    def website_validator(self, proxy):
+        """
+        likes ip_validator except uses website like baidu
+        """
+        url = random.choice(WEBSITES)
+        resp = request(url, proxy)
+        if not resp:
+            return None
+        if resp.status_code == 200:
+            self.logger.info('website validator: available proxy - %s', proxy)
+            return proxy
+        self.logger.error('website validator: useless proxy %s', proxy)
