92c8c51d739d4fe346544537b916ebe237f064e9
broholens <broholens@outlook.com>
2017-09-26 14:36:46 +0800

    use utils.gevent_exe

--- ipproxy/validator.py
+++ ipproxy/validator.py
@@ -4,22 +4,16 @@ validate whether ip is useful or not
 created at 2017-9-25 by broholens
 """
 
-from gevent import monkey; monkey.patch_all()
-import re
 import random
-import gevent
-from pymongo import MongoClient
-from ipproxy import settings
-from ipproxy.utils import request
-
-HOST = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+from ipproxy.settings import (DB, TEST_SITES, HOST, WEBSITES)
+from ipproxy.utils import request, exe_tasks
 
 
 class Validator:
 
     def __init__(self):
         # result of httpbin is different from ip181
-        self.db = MongoClient(settings.MONGO).ipproxy
+        self.db = DB
         self.validator = {
             'good': ip_validator,
             'available': website_validator
@@ -30,16 +24,11 @@ class Validator:
         self._validate(proxy_list, 'available')
 
     def _validate(self, proxy_list, key='good'):
-        tasks = [
-            gevent.spawn(self.validator.get(key), proxy) for proxy in proxy_list
-        ]
-        gevent.joinall(tasks)
-        for task in tasks:
-            if task.value:
-                self.storage(task.value, key)
+        for proxy in exe_tasks(self.validator.get(key), proxy_list):
+            self.storage(proxy, key)
 
     def storage(self, proxy, key='good'):
-        self.db.proxies.update_one(
+        self.db.validator.update_one(
             {'proxy': proxy},
             {
                 '$set': {'proxy': proxy},
@@ -57,7 +46,9 @@ def ip_validator(proxy):
     :param proxy: something like {'http': 'http://162.243.107.120:3128'}
     :return: proxy if usable else None
     """
-    url = random.choice(settings.TEST_SITES)
+    if proxy is None:
+        return None
+    url = random.choice(TEST_SITES)
     resp = request(url, proxy)
     if not resp:
         return None
@@ -70,7 +61,7 @@ def website_validator(proxy):
     """
     likes ip_validator except uses website like baidu
     """
-    url = random.choice(settings.WEBSITES)
+    url = random.choice(WEBSITES)
     resp = request(url, proxy)
     if resp and resp.status_code == 200:
         return proxy
