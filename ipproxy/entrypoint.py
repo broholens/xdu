632615e628583077a2602bc8ad3124c48f94a571
broholens <broholens@outlook.com>
2017-10-22 09:14:18 +0800

    add collect_validate()

--- ipproxy/entrypoint.py
+++ ipproxy/entrypoint.py
@@ -3,30 +3,41 @@ collect and validate ip
 created at 2017-9-26 by broholens
 """
 
+import time
+from multiprocessing import Process
+
 from ipproxy.collector import Collector
 from ipproxy.validator import Validator
-from ipproxy.settings import DB
+from ipproxy.settings import Q
 
 
-class IpProxy:
+class IPProxy:
 
-    def __init__(self):
-        self.collector = Collector()
-        self.validator = Validator()
-        self.db = DB
+    @staticmethod
+    def collect():
+        Collector().collect()
 
-    def collect(self):
-        self.collector.collect()
+    @staticmethod
+    def validate():
+        Validator().validate()
 
-    def validate(self, proxy_list, key='good'):
-        self.validator.validate(proxy_list, key)
-
-    def get_db_proxies(self, collection='collector'):
-        for proxy in self.db[collection].find({}, {'proxy': 1, '_id': 0}):
-            yield proxy.get('proxy')
+    @staticmethod
+    def collect_validate():
+        collector = Collector()
+        validator = Validator()
+        p1 = Process(target=collector.collect)
+        p2 = Process(target=validator.website_validate, args=(Q,))
+        p3 = Process(target=validator.test_validate)
+        p1.daemon, p3.daemon = True, True
+        p1.start()
+        time.sleep(10)
+        p2.start()
+        p3.start()
+        p2.join()
 
 
 if __name__ == '__main__':
-    ip = IpProxy()
-    proxies = ip.get_db_proxies()
-    ip.validate(proxies, 'available')
+    ip = IPProxy()
+    # ip.collect()
+    # ip.validate()
+    ip.collect_validate()
