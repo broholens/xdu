860789a6b644bdc9d0aaa934d1bcbb11017fb71b
broholens <broholens@outlook.com>
2017-10-25 07:28:47 +0800

    more ip source

--- ipproxy/collector.py
+++ ipproxy/collector.py
@@ -8,7 +8,7 @@ from ipproxy.utils import (
     exception_handler as eh
 )
 from ipproxy.settings import (
-    RE_IP, RE_IP_2, DATA5U,
+    RE_IP, DATA5U, XS_START, XS_END,
     PROXY_SITES, COLLECTION, PAGES, Q, ZDAYE_START_PAGE_ID,
 )
 
@@ -20,25 +20,17 @@ class Collector:
         self.q = queue.Queue()
         self.put_to_queue()
 
-    def _storage(self, proxy, response_url):
-        # protocol = 'http' if proxy.get('http') else 'https'
-        COLLECTION.update_one(
-            {'proxy': proxy},
-            {
-                '$set': {'proxy': proxy},
-                '$push': {'source': response_url},
-                '$currentDate': {'lastModified': True}
-            },
-            upsert=True
-        )
-
-    def storage(self, ips, response_url):
+    def storage(self, ips):
         if not ips:
             return
         for ip in ips:
             proxy = {'http': f'http://{ip}', 'https': f'https://{ip}'}
             Q.put(proxy)
-            self._storage(proxy, response_url)
+            COLLECTION.update_one(
+                {'proxy': proxy},
+                {'$set': {'proxy': proxy}},
+                upsert=True
+            )
 
     def put_to_queue(self):
 
@@ -52,7 +44,7 @@ class Collector:
         # for data5u
         # urls.extend(
         #     [(f'http://www.data5u.com/dayip-{i}-10.html', self.parse_data5u)
-        #      for i in range(1, sum((1, PAGES)))]
+        #      for i in range(1, sum((1, 290)))]
         # )
 
         # for ip3366
@@ -63,89 +55,108 @@ class Collector:
         #      for j in range(1, 5)]
         # )
 
-        # for httpdaili
-        # urls.append(('http://www.httpdaili.com/mfdl/', self.parse_regex_2))
-
         # for mayi
-        # now = day_delta()
+        # now = day_delta() - 18
         # urls.extend(
         #     [(f'http://www.mayidaili.com/share/view/{i}/', self.parse_regex)
-        #      for i in range(now, now - PAGES, -1)]
+        #      # for i in range(now, now - PAGES, -1)]
+        #      for i in range(now, 1, -1)]
         # )
 
         # for kuaidaili
-        # urls.extend(
-        #     [(f'http://www.kuaidaili.com/free/inha/{i}', self.parse_kuai)
-        #      for i in range(1, sum((1, PAGES)))] +
-        #     [(f'http://www.kuaidaili.com/free/intr/{i}', self.parse_kuai)
-        #      for i in range(1, sum((1, PAGES)))]
-        # )
+        urls.extend(
+            [(f'http://www.kuaidaili.com/free/inha/{i}', self.parse_regex)
+             for i in range(1, sum((1, 1000)))]  # +
+            # [(f'http://www.kuaidaili.com/free/intr/{i}', self.parse_kuai)
+            #  for i in range(1, sum((1, PAGES)))]
+        )
 
         # for zdaye
-        # now = hour_delta() + ZDAYE_START_PAGE_ID
-        # urls.extend(
-        #     [(f'http://ip.zdaye.com/dayProxy/ip/{i}.html', self.parse_regex)
-        #      for i in range(now, now - PAGES, -1)]
-        # )
+        now = hour_delta() + ZDAYE_START_PAGE_ID
+        urls.extend(
+            [(f'http://ip.zdaye.com/dayProxy/ip/{i}.html', self.parse_regex)
+             for i in range(now, now - 795, -1)]
+        )
 
-        # for ip181
+        # for ip181+
         # urls.extend(
-        #     [(f'http://www.ip181.com/daili/{i}.html', self.parse_regex_2)
-        #      for i in range(1, sum((1, PAGES)))]
+        #     [(f'http://www.ip181.com/daili/{i}.html', self.parse_regex)
+        #      for i in range(1, sum((1, 690)))]
         # )
         #
         # # for xici
         # urls.extend(
-        #     [(f'http://www.xicidaili.com/nn/{i}', self.parse_xici)
-        #      for i in range(1, sum((1, PAGES)))]
+        #     [(f'http://www.xicidaili.com/nn/{i}', self.parse_regex)
+        #      for i in range(1, sum((1, 1000)))]
         # )
         #
         # # for mimi
         # urls.extend(
         #     [(f'http://www.mimiip.com/gngao/{i}', self.parse_regex_2)
-        #      for i in range(1, sum((1, PAGES)))] +
+        #      for i in range(1, sum((1, 680)))] +
         #     [(f'http://www.mimiip.com/gnpu/{i}', self.parse_regex_2)
-        #      for i in range(1, sum((1, PAGES)))]
+        #      for i in range(1, sum((1, 100)))]
         # )
+
+        # for xiaoshu+
+        # now = day_delta(XS_START, XS_END) * 2 + 27
+        # urls.extend(
+        #     [(f'http://www.xsdaili.com/dayProxy/ip/{i}.html', self.parse_regex)
+        #      for i in range(now, 1, -1)]  # not contains 1
+        # )
+
+        # for goubanjia
+        # 'http://www.goubanjia.com/free/'
+
+        # for iphai
+        # 'http://daili.iphai.com/
+
+        # for xiaoma
+        # 'http://yun-daili.com/free.asp'
+
+        # for unknown
+        # 'http://ip.baizhongsou.com/'
+
+        # for pachong
+        # 'http://www.pcdaili.com/index.php'
 
         random.shuffle(urls)
         for url in urls:
             self.q.put(url)
 
     def parse_regex(self, response):
-        self.storage(RE_IP.findall(response.text), response.url)
+        ips = RE_IP.findall(response.text)
+        if '</td>' not in ips[0] and ':' in ips[0]:
+            self.storage(ips)
+            return
 
-    def parse_regex_2(self, response):
-        ips = list()
-        for ip in RE_IP_2.findall(response.text):
-            host, port = ip.split('<', 1)
-            port = port.split('>')[-1]
-            ips.append(f'{host}:{port}')
-        self.storage(ips, response.url)
+        ips = (f'{ip.split("<")[0]}:{ip.split(">")[-1]}' for ip in ips)
+        self.storage(ips)
 
     def parse_data5u(self, response):
         for url in DATA5U.findall(response.text):
             self.q.put((url, self.parse_regex))
 
-    def parse_kuai(self, response):
-        parser = Parser(response,
-                        '//*[@id="list"]/table/tbody/tr',
-                        './td[@data-title="IP"]/text()',
-                        './td[@data-title="PORT"]/text()'
-                        )
-        ips = parser.parse(self.q, self.parse_kuai)
-        self.storage(ips, response.url)
+    # def parse_kuai(self, response):
+    #     parser = Parser(response,
+    #                     '//*[@id="list"]/table/tbody/tr',
+    #                     './td[@data-title="IP"]/text()',
+    #                     './td[@data-title="PORT"]/text()'
+    #                     )
+    #     ips = parser.parse(self.q, self.parse_kuai)
+    #     self.storage(ips, response.url)
 
-    def parse_xici(self, response):
-        parser = Parser(response,
-                        '//table[@id="ip_list"]/tr[position()>2]',
-                        './td[2]/text()',
-                        './td[3]/text()'
-                        )
-        ips = parser.parse(self.q, self.parse_xici)
-        self.storage(ips, response.url)
+    # def parse_xici(self, response):
+    #     parser = Parser(response,
+    #                     '//table[@id="ip_list"]/tr[position()>2]',
+    #                     './td[2]/text()',
+    #                     './td[3]/text()'
+    #                     )
 
-    def collect(self, concurrent_num=3):
+    #     ips = parser.parse(self.q, self.parse_xici)
+    #     self.storage(ips, response.url)
+
+    def collect(self, concurrent_num=10):
         while not self.q.empty():
             elements = cut_queue(self.q, concurrent_num)
             reqs = (request(ele[0], is_map=True) for ele in elements)
@@ -157,4 +168,4 @@ class Collector:
                     continue
                 self.logger.info('fetched %s', resp.url)
                 element[1](resp)
-            # time.sleep(30)
+            # time.sleep(3)
