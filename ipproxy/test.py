ba9e4c04ec70a6c804c1e8087cb586fe6049fd16
broholens <broholens@outlook.com>
2017-10-25 07:26:44 +0800

    rewrite Collector to use crawled proxies

--- ipproxy/test.py
+++ ipproxy/test.py
@@ -6,13 +6,43 @@ import grequests
 
 from ipproxy.collector import Collector
 from ipproxy.utils import request, cut_queue
+from ipproxy.settings import RE_HOST
 
 
 api_get_url = 'http://0.0.0.0:8080/get'
 api_detect_url = 'http://0.0.0.0:8080/increase_detect_times'
 api_alive_url = 'http://0.0.0.0:8080/increase_alive_times'
 
-urls = [f'http://www.xicidaili.com/nn/{i}' for i in range(1, sum((1, 20)))]
+urls = list()
+
+
+# for data5u
+# urls.extend(
+#     [f'http://www.data5u.com/dayip-{i}-10.html' for i in range(1, sum((1, 290)))]
+# )
+
+# for ip3366
+# TODO: network firewall  (head/title -> network firewall)
+urls.extend(
+    [f'http://www.ip3366.net/free/?stype={b}/page={c}'
+     for c in range(1, sum((1, 7)))
+     for b in range(1, 5)]
+)
+
+
+# for xici
+urls.extend(
+    [f'http://www.xicidaili.com/nn/{d}'
+     for d in range(1, sum((101, 1000)))]
+)
+
+# for mimi
+urls.extend(
+    [f'http://www.mimiip.com/gngao/{e}'
+     for e in range(1, sum((1, 680)))] +
+    [f'http://www.mimiip.com/gnpu/{f}'
+     for f in range(1, sum((1, 100)))]
+)
 
 
 class MyCollector(Collector):
@@ -40,18 +70,20 @@ class MyCollector(Collector):
                     for u, p in zip(_urls, proxies))
             resps = grequests.map(reqs, gtimeout=10)
             for url, resp, proxy in zip(_urls, resps, proxies):
-                if not resp:
+                if not resp or len(RE_HOST.findall(resp.text)) < 5:
                     self.logger.error('recycle: %s', url)
                     self.q.put(url)
                     requests.post(api_detect_url, data=json.dumps(proxy))
                     continue
                 self.logger.info('fetched: %s', url)
-                self.logger.info('%s', resp.text)
+                self.logger.info('%s', RE_HOST.findall(resp.text))
                 requests.post(api_alive_url, data=json.dumps(proxy))
-                self.parse_regex_2(resp)
+                self.parse_regex(resp)
 
 
 if __name__ == '__main__':
     my_collector = MyCollector(urls)
     my_collector.collect()
+    # c = Collector()
+    # c.collect()
 
